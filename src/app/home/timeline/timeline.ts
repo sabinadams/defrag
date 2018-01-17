ddb23d7f26e5dcae804baf9fa9fb05ccd18735ae
Sabin <sabintheworld@gmail.com>
2018-01-17 00:10:51 -0800

    Began new layout

--- src/app/home/timeline/timeline.ts
+++ src/app/home/timeline/timeline.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { TimelineService } from '../shared/services/timeline-service';
 import { ITimelineItem } from '../../shared/models/TimelineItem';
 import { Observable } from 'rxjs/Observable';
-
 @Component({
   selector: 'timeline',
   templateUrl: './timeline.html',
@@ -10,7 +9,9 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TimelineComponent implements OnInit {
   feed_items: Observable<ITimelineItem[]>;
-  constructor( private _timelineService: TimelineService ) {}
+  constructor( private _timelineService: TimelineService, public el: ElementRef ) {
+   
+  }
   ngOnInit() {
       this.feed_items = this._timelineService.populateFeed();     
   }
