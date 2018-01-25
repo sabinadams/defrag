9d50e2b6009dc3c20e7537797eaa7052cf792eab
Sabin <sabintheworld@gmail.com>
2018-01-24 21:19:22 -0800

    JS Cleanup
    Got rid of the 'nilla JS stuff in timeline scroller

--- src/app/timeline/timeline.ts
+++ src/app/timeline/timeline.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { TimelineService } from './shared/services/timeline-service';
 import { ITimelineItem } from './../shared/models/TimelineItem';
 import { Observable } from 'rxjs/Observable';
@@ -11,31 +11,27 @@ export class TimelineComponent implements OnInit {
   feed_items: Observable<ITimelineItem[]>;
   scrolling: boolean = false;
   scrollInterval: any;
-  constructor( private _timelineService: TimelineService, public el: ElementRef ) {
-   
-  }
+  @ViewChild('timeline') el: ElementRef; 
+
+  constructor( private _timelineService: TimelineService, public el: ElementRef ) {}
+  
   ngOnInit() {
-      this.feed_items = this._timelineService.populateFeed();     
+    this.feed_items = this._timelineService.populateFeed(); 
   }
 
   handleScroll(e) {
     // Number correlates to sm-break in SCSS, 17 correlates to scrollbar padding workaround width
     if ( screen.width - 700 + 17 <= self.innerWidth ) {
       e.preventDefault();
-      document.getElementById("tct").scrollLeft += e.deltaY ? e.deltaY : e.detail * 25;
+      this.el.nativeElement.scrollLeft += e.deltaY ? e.deltaY : e.detail * 25;
     }
-  }
-
-  scrollToItem(number) {
-   document.getElementById(`timeline-item-${number}`)
-    .scrollIntoView({behavior: 'smooth',  inline: 'center'});
   }
 
   toggleScroll() {
     this.scrolling = !this.scrolling;
     if(this.scrolling) {
       this.scrollInterval = setInterval(() => {
-        document.getElementById("tct").scrollLeft += 2;
+        this.el.nativeElement.scrollLeft += 2;
       }, 20);
     } else {
       clearInterval(this.scrollInterval);
